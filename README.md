f262ded5fae677abe366b99b0749ec1830c94092
stvnreis <stvnreis@gmail.com>
2023-08-12 20:03:51 -0300

    adicionado imagem postgresql e README

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # bd2-atividade01
+
+## Subindo um container no Docker com uma imagem do postgresql
+
+### Passo 01
+Instalar [DockerDesktop](https://www.docker.com/products/docker-desktop/) e seguir o passo a passo para iniciar o processo corretamente
+
+### Passo 02
+Execute o DockerDesktop e assim que terminar de iniciar o processo, abra o terminal no diretorio deste projeto e execute o seguinte comando
+
+```bash
+docker-compose up -d
+```
+
+### Passo 03
+Execute seu SGBD e configure uma nova conexão com os seguintes parâmetros:
+
+Datasource: PostgreSQL
+Host: localhost
+Port: 5438
+Authentication: User & Password
+User: postgres
+Password: postgres
--- trabalho01.sql
+++ trabalho01.sql
@@ -1,53 +1,46 @@
 create table Equipe (id serial constraint pk_equipe primary key, nome varchar(100));
-go
+
 create table Campeonato (id serial constraint pk_campeonato primary key, nome varchar(100));
-go
+
 create table EquipeCampeonato (
   id serial constraint pk_equipeCampeonato primary key,
-  idEquipe integer,
-  idCampeonato integer,
+  idEquipe integer constraint fk_equipeCampeonato_equipe references Equipe(id),
+  idCampeonato integer constraint fk_equipeCampeonato_campeonato references Campeonato(id),
   posicao integer
 );
-go
+
 create table Cidade (id serial constraint pk_cidade primary key, nome varchar(100));
-go
 create table tecnico (
-  id serial constraint pk_equipe primary key,
+  id serial constraint pk_tecnico primary key,
   cpf varchar(100),
   nome varchar(50),
-  idEquipe int constraint pk_tecnico_equipe foreign key references equipe(id),
+  idEquipe int constraint pk_tecnico_equipe references equipe(id),
   dataNascimento date
 );
-go
 
 create table jogadorBrasileiro (
   id serial constraint pk_jogadorBrasileiro primary key,
   cpf varchar(14),
   nome varchar(50),
   posicao varchar(50),
-  idEquipe int constraint pk_jogadorBrasileiro_equipe foreign key references equipe(id),
+  idEquipe int constraint pk_jogadorBrasileiro_equipe references equipe(id),
   salario decimal(15,2)
 );
 
 create table jogadorEstrangeiro (
-  id serial constraint pk_jogadorBrasileiro primary key,
+  id serial constraint pk_jogadorEstrangeiro primary key,
   passaporte varchar(50),
   nome varchar(50),
   posicao varchar(50),
-  idEquipe int constraint pk_jogadorBrasileiro_equipe foreign key references equipe(id),
+  idEquipe int constraint pk_jogadorBrasileiro_equipe references equipe(id),
   paisOrigem varchar(50),
   salario decimal(15,2)
 );
 
 create table paisesTecnicos (
-  	idTecnico int constraint fk_paisesTecnicos_tecnico references tecnico,
-  	pais varchar(50),
-  	primary key (idTecnico)
- );
-Paulo Henrique
-moschenhenrique@hotmail.com
-
-		insert into scientist (id, firstname, lastname) values (1, 'albert', 'einstein');
-		insert into scientist (id, firstname, lastname) values (2, 'isaac', 'newton');
-		insert into scientist (id, firstname, lastname) values (3, 'marie', 'curie');
-		select * from scientist;
+  	idTecnico int constraint fk_paisesTecnicos_tecnico references tecnico primary key,
+  	pais varchar(50)
+);
+insert into Equipe(nome) values('palmeiras');
+insert into tecnico(cpf, nome, idequipe, datanascimento) values('1234567890123','Steven', 1, '2000-01-01');
+insert into paisesTecnicos(idTecnico, pais) values(1, 'Brasil');
